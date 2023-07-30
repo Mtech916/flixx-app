22f887c0a78908f364bfa0a012b1b35759de6fd5
Mtech916 <shredtechfit@gmail.com>
2023-07-30 09:44:56 -0700

    Replaced movie details with dynamic data

--- js/script.js
+++ js/script.js
@@ -68,6 +68,63 @@ async function displayPopularShows() {
   })
 }
 
+// Display Movie Details
+async function displayMovieDetails() {
+  const movieID = window.location.search.split('=')[1];
+
+  const movie = await fetchAPIData(`/movie/${movieID}`);
+  console.log(movie)
+
+  const div = document.createElement('div');
+  div.innerHTML = `
+    <div class="details-top">
+    <div>
+    ${
+      movie.poster_path
+        ? `<img
+        src="https://image.tmdb.org/t/p/w500${movie.poster_path}"
+        class="card-img-top"
+        alt="${movie.title}"
+        />` : `<img
+        src="../images/no-image.jpg"
+        class="card-img-top"
+        alt="${movie.title}"
+        />`
+    }
+    </div>
+    <div>
+      <h2>${movie.title}</h2>
+      <p>
+        <i class="fas fa-star text-primary"></i>
+        ${movie.vote_average.toFixed(1)} / 10
+      </p>
+      <p class="text-muted">Release Date: ${movie.release_date}</p>
+      <p>
+      ${movie.overview}
+      </p>
+      <h5>Genres</h5>
+      <ul class="list-group">
+        <li>${movie.genres.map((genre) => `<li>${genre.name}</li>`).join('')}</li>
+      </ul>
+      <a href="${movie.homepage}" target="_blank" class="btn">Visit Movie Homepage</a>
+    </div>
+    </div>
+    <div class="details-bottom">
+      <h2>Movie Info</h2>
+      <ul>
+        <li><span class="text-secondary">Budget:</span> $${addCommasToNumber(movie.budget)}</li>
+        <li><span class="text-secondary">Revenue:</span> $${addCommasToNumber(movie.revenue)}</li>
+        <li><span class="text-secondary">Runtime:</span> ${movie.runtime} minutes</li>
+        <li><span class="text-secondary">Status:</span> ${movie.status}</li>
+      </ul>
+      <h4>Production Companies</h4>
+      <div class="list-group">${movie.production_companies.map((company) => `<span>${company.name}</span>`).join('')}</div>
+    </div>
+  `;
+
+  document.querySelector('#movie-details').appendChild(div);
+}
+
 // Fetch Data from TMDB API
 async function fetchAPIData(endpoint) {
   // Register my key at https://www.themoviedb.org/settings/api and enter here
@@ -106,6 +163,9 @@ function highlightActiveLink() {
   });
 }
 
+function addCommasToNumber(number) {
+  return number.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+}
 
 // Init App
 function init() {
@@ -118,7 +178,7 @@ function init() {
       displayPopularShows();
       break;
     case '/movie-details.html':
-      console.log('Movie Details Page');
+      displayMovieDetails();
       break;
     case '/tv-details.html':
       console.log('TV Details Page');
