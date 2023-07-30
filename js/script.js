7c101f6ece3549fc310fb2d5da64aff894dbc88b
Mtech916 <shredtechfit@gmail.com>
2023-07-30 09:55:50 -0700

    Added movie backdrop overlay background to details page

--- js/script.js
+++ js/script.js
@@ -73,7 +73,9 @@ async function displayMovieDetails() {
   const movieID = window.location.search.split('=')[1];
 
   const movie = await fetchAPIData(`/movie/${movieID}`);
-  console.log(movie)
+
+  // Overlay for background image
+  displayBackgroundImage('movie', movie.backdrop_path);
 
   const div = document.createElement('div');
   div.innerHTML = `
@@ -123,6 +125,28 @@ async function displayMovieDetails() {
   `;
 
   document.querySelector('#movie-details').appendChild(div);
+}
+
+// Dislay Backdrop On Details Pages
+function displayBackgroundImage(type, backgroundPath) {
+  const overlayDiv = document.createElement('div');
+  overlayDiv.style.backgroundImage = `url('https://image.tmdb.org/t/p/original/${backgroundPath}')`;
+  overlayDiv.style.backgroundSize = 'cover';
+  overlayDiv.style.backgroundPosition = 'center';
+  overlayDiv.style.backgroundRepeat = 'no-repeat';
+  overlayDiv.style.height = '100vh';
+  overlayDiv.style.width = '100vw';
+  overlayDiv.style.position = 'absolute';
+  overlayDiv.style.top = '0';
+  overlayDiv.style.left = '0';
+  overlayDiv.style.zIndex = '-1';
+  overlayDiv.style.opacity = '0.3';
+
+  if (type === 'movie') {
+    document.querySelector('#movie-details').appendChild(overlayDiv);
+  } else {
+    document.querySelector('#show-details').appendChild(overlayDiv);
+  }
 }
 
 // Fetch Data from TMDB API
