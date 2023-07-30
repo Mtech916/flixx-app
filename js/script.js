6581c081631e33890ac6f5211f26ee0c98c2dc62
Mtech916 <shredtechfit@gmail.com>
2023-07-30 13:06:11 -0700

    created search functionality

--- js/script.js
+++ js/script.js
@@ -1,5 +1,17 @@
 const global = {
   currentPage: window.location.pathname,
+  search: {
+    term: '',
+    type: '',
+    page: 1,
+    totalPages: 1,
+  },
+  api: {
+    // Register my key at https://www.themoviedb.org/settings/api and enter here
+    // Only use this for development or very small projects. I should store my key and  make requests from a server
+    apiKey: '98fb5a79fd8db0d1e3c3b68ad433c690',
+    apiUrl: 'https://api.themoviedb.org/3/'
+  }
 };
 
 // Display 20 most Popular Movies
@@ -207,6 +219,22 @@ function displayBackgroundImage(type, backgroundPath) {
   }
 }
 
+// Search Movies/Shows
+async function search() {
+  const queryString = window.location.search;
+  const urlParams = new URLSearchParams(queryString);
+  
+  global.search.type = urlParams.get('type');
+  global.search.term = urlParams.get('search-term');
+
+  if (global.search.term !== '' && global.search.term !== null) {
+    const results = await searchAPIData();
+    console.log(results);
+  } else {
+    showAlert('Please enter a search term');
+  }
+}
+
 // Display Slider Movies
 async function displaySlider() {
   const { results } = await fetchAPIData('/movie/now_playing');
@@ -256,18 +284,34 @@ function initSwiper() {
 
 // Fetch Data from TMDB API
 async function fetchAPIData(endpoint) {
-  // Register my key at https://www.themoviedb.org/settings/api and enter here
-  // Only use this for development or very small projects. I should store my key and  make requests from a server
-  const API_KEY = '98fb5a79fd8db0d1e3c3b68ad433c690';
-  const BASE_URL = 'https://api.themoviedb.org/3';
+  const API_KEY = global.api.apiKey;
+  const API_URL = global.api.apiUrl;
 
   showSpinner();
 
-  const response = await fetch(`${BASE_URL}${endpoint}?api_key=${API_KEY}&language=en-US`);
+  const response = await fetch(`${API_URL}${endpoint}?api_key=${API_KEY}&language=en-US`);
 
   const data = await response.json();
 
-  hisdeSpinner();
+  hideSpinner();
+
+  return data;
+}
+
+// Make Request To Search
+async function searchAPIData() {
+  const API_KEY = global.api.apiKey;
+  const API_URL = global.api.apiUrl;
+
+  showSpinner();
+
+  const response = await fetch(
+    `${API_URL}search/${global.search.type}?api_key=${API_KEY}&language=en-US&query=${global.search.term}`
+  );
+
+  const data = await response.json();
+
+  hideSpinner();
 
   return data;
 }
@@ -276,7 +320,7 @@ function showSpinner() {
   document.querySelector('.spinner').classList.add('show');
 }
 
-function hisdeSpinner() {
+function hideSpinner() {
   document.querySelector('.spinner').classList.remove('show');
 }
 
@@ -290,6 +334,16 @@ function highlightActiveLink() {
       link.classList.remove('active');
     }
   });
+}
+
+// Show Alert
+function showAlert(message, className) {
+  const alertEl = document.createElement('div');
+  alertEl.classList.add('alert', className);
+  alertEl.appendChild(document.createTextNode(message));
+  document.querySelector('#alert').appendChild(alertEl);
+
+  setTimeout(() => alertEl.remove(), 3000);
 }
 
 function addCommasToNumber(number) {
@@ -314,7 +368,7 @@ function init() {
       displayShowDetails();
       break;
     case '/search.html':
-      console.log('Search Page');
+      search();
       break;
   }
 
